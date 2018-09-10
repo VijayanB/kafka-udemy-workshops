f970549ddb0d74ea260bd8722a0dbb2bbc715803
Vijayan Balasubramanian <VBalasubramanian@walmartlabs.com>
2018-09-09 18:33:35 -0700

    Lecture 43- Season 6

---
+++ src/main/java/com/github/kafka/ProducerDemo.java
@@ -0,0 +1,10 @@
+package com.github.kafka;
+
+/**
+ * Created by v0b004g on 9/9/18.
+ */
+public class ProducerDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
